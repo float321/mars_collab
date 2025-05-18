80ed7af0f1a84cba5b8d13dbe2b013ea467cbed0
float321 <187214682+float321@users.noreply.github.com>
2025-05-18 23:43:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import json
+import random
+from flask import Flask, render_template
 
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def basee():
+    return render_template('base.html')
+
+
+@app.route('/member')
+def member():
+    with open('templates/crew.json', encoding='utf-8') as f:
+        crew = json.load(f)
+    random_member = random.choice(crew)
+    return render_template('member.html', member=random_member)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
